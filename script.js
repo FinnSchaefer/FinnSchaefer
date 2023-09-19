9f1f4be715989253bcd299ee009f04e5d7bfbe81
Finn <finn.pm.schaefer@gmail.com>
2023-09-19 15:20:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 const githubProjectsContainer = document.getElementById('github-projects');
 
-// Fetch your GitHub projects using the GitHub API (replace with your GitHub username)
-const githubUsername = 'FinnSchaefer';
+// Fetch GitHub projects using the GitHub API (replace with your GitHub username)
+const githubUsername = 'your-github-username';
 const apiUrl = `https://api.github.com/users/${githubUsername}/repos`;
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        data.forEach(project => {
+        // Filter out the "FinnSchaefer" repo
+        const filteredProjects = data.filter(project => project.name !== 'FinnSchaefer');
+
+        filteredProjects.forEach(project => {
             const projectCard = document.createElement('div');
             projectCard.classList.add('project-card');
-            
+
             const projectName = document.createElement('h3');
             projectName.textContent = project.name;
-            
+
             const projectDescription = document.createElement('p');
             projectDescription.textContent = project.description || 'No description available.';
-            
+
             projectCard.appendChild(projectName);
             projectCard.appendChild(projectDescription);
-            
+
             githubProjectsContainer.appendChild(projectCard);
         });
     })
     .catch(error => {
         console.error('Error fetching GitHub projects:', error);
     });
-
- // script.js
 
 // Function to check if an element is in the viewport
 function isElementInViewport(element) {
